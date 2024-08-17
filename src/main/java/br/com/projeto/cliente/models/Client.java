8440c17e274251052f0f3f94fee34a7358920c5d
tamirescomotti <tamiresalvescontatos@gmail.com>
2024-08-17 00:04:43 -0300

    mapeando classes model

---
+++ src/main/java/br/com/projeto/cliente/models/Client.java
@@ -0,0 +1,61 @@
+package br.com.projeto.cliente.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@Table(name = "tb_clients")
+public class Client extends AbstractEntity{
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String cpf;
+    @NotEmpty
+    private String email;
+    private String adress;
+    private String cep;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
